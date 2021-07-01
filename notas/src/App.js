9344437935dd89e19ca54d6ec9df9462fd9df367
Juanda <danielalejandre1050@yahoo.com>
2021-07-01 17:43:43 -0500

    Empezar Navbar

--- notas/src/App.js
+++ notas/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import { Navbar } from './Navbar/Navbar';
+import { Navbar } from './components/Navbar/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <main>
-        <Switch>
-
-        </Switch>
-      </main>
-    </Router>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Navbar />
+        <main>
+          <Switch>
+  
+          </Switch>
+        </main>
+      </Router>
+    );
+  }
+  }
 
 export default App;
